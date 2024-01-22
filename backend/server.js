b305a1b1cca5b06665741365049033a5905034bb
Rohan <rohankumar5803@gmail.com>
2024-01-22 20:23:21 +0530

    mongodb connection established

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -9,6 +10,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true});
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log("MongoDB database connection successfully established.");
+})
+
 app.listen(port, ()=>{
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${port}...`);
 });
