f996ed9984be1ca7348f1b9f53fa72ce03e6aba9
Rohan <rohankumar5803@gmail.com>
2024-02-01 21:24:31 +0530

    updated users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -15,4 +15,7 @@ router.route('/add').post((req, res) => {
     newUser.save()
     .then(()=> res.json('User added!'))
     .catch(err => res.status(400).json('Error: '+ err));
-})
+});
+
+module.exports = router;
+
