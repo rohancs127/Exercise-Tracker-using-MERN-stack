dabee2642eb1d9b7a4bbf8c998d9806d770329cf
Rohan <rohankumar5803@gmail.com>
2024-03-27 21:45:03 +0530

    installs and imports react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return <>Hello</>;
