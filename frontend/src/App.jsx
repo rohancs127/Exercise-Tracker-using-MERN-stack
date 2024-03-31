60ae000caf3bf3db6887efa250a14bd74392ce84
Rohan <rohankumar5803@gmail.com>
2024-03-31 18:17:19 +0530

    added route path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,21 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import ExercisesList from "./Components/ExercisesList";
+import EditExercise from "./Components/EditExercise";
+import CreateExercise from "./Components/CreateExercise";
+import CreateUser from "./Components/CreateExercise";
 
 function App() {
-  return(
+  return (
     <Router>
-      <div>Hello world</div>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" exact component={EditExercise} />
+      <Route path="/create" exact component={CreateExercise} />
+      <Route path="/user" exact component={CreateUser} />
     </Router>
   );
 }
