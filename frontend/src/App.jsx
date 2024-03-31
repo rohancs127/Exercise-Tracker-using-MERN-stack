046f862cb4b2dac4c55b9834a6650846f06a3ed1
Rohan <rohankumar5803@gmail.com>
2024-03-31 18:20:28 +0530

    sets up code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,14 +10,14 @@ import CreateExercise from "./Components/CreateExercise";
 import CreateUser from "./Components/CreateExercise";
 
 function App() {
-  return (
+  return(
     <Router>
-      <Navbar />
-      <br />
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" exact component={EditExercise} />
-      <Route path="/create" exact component={CreateExercise} />
-      <Route path="/user" exact component={CreateUser} />
+      <Navbar/>
+      <br/>
+      <Route path="/" exact component={ExercisesList}/>
+      <Route path="/edit/:id" exact component={EditExercise}/>
+      <Route path="/create" exact component={CreateExercise}/>
+      <Route path="/user" exact component={CreateUser}/>
     </Router>
   );
 }
