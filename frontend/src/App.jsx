98621a539ad3b8b9ed56e7c334d33f1a1f4d5c20
Rohan <rohankumar5803@gmail.com>
2024-04-01 22:36:29 +0530

    addsroutes component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import ExercisesList from "./Components/ExercisesList";
 import EditExercise from "./Components/EditExercise";
@@ -14,10 +14,12 @@ function App() {
     <Router>
       <Navbar/>
       <br/>
+      <Routes>
       <Route path="/" exact component={ExercisesList}/>
       <Route path="/edit/:id" exact component={EditExercise}/>
       <Route path="/create" exact component={CreateExercise}/>
       <Route path="/user" exact component={CreateUser}/>
+      </Routes>
     </Router>
   );
 }
