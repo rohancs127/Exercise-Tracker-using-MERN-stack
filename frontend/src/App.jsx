46e7524bc96a093f381c2d1aa063db6757e56cd6
Rohan <rohankumar5803@gmail.com>
2024-04-06 20:30:40 +0530

    fixes route issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,15 +10,15 @@ import CreateExercise from "./Components/CreateExercise";
 import CreateUser from "./Components/CreateExercise";
 
 function App() {
-  return(
+  return (
     <Router>
-      <Navbar/>
-      <br/>
+      <Navbar />
+      <br />
       <Routes>
-      <Route path="/" exact component={ExercisesList}/>
-      <Route path="/edit/:id" exact component={EditExercise}/>
-      <Route path="/create" exact component={CreateExercise}/>
-      <Route path="/user" exact component={CreateUser}/>
+        <Route path="/" element={<ExercisesList />} />
+        <Route path="/edit/:id" element={<EditExercise />} />
+        <Route path="/create" element={<CreateExercise />} />
+        <Route path="/user" element={<CreateUser />} />
       </Routes>
     </Router>
   );
