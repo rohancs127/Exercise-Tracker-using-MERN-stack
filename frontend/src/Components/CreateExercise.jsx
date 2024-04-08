2db78fa511437cff72084aa952e5ad1958750d2c
Rohan <rohankumar5803@gmail.com>
2024-04-08 22:06:04 +0530

    create hook for setuser

--- frontend/src/Components/CreateExercise.jsx
+++ frontend/src/Components/CreateExercise.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 function CreateExercise() {
+  const [user, setUser] = useState({
+    username: "",
+    description: "",
+    duration: 0,
+    date: new Date(),
+    users: [],
+  });
+
+  const onChangeusername = (e, date) => {
+    setUser({
+      username: e.target.value,
+      description: e.target.value,
+      duration: e.target.value,
+      date: date,
+    });
+  };
+
   return <div>CreateExercise</div>;
 }
 
