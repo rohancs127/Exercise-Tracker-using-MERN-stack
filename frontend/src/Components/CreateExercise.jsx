869e4b7210ddb7242e4e3b72d60b36446f68eca9
Rohan <rohankumar5803@gmail.com>
2024-04-10 15:12:51 +0530

    some fixes yet to be made

--- frontend/src/Components/CreateExercise.jsx
+++ frontend/src/Components/CreateExercise.jsx
@@ -19,12 +19,77 @@ function CreateExercise() {
     });
   };
 
+  const onSubmit = () => {};
+
+  const onChangeDescription = () => {};
+
+  const onChangeDuration = () => {};
+
+  const onChangeDate = () => {};
+
   const exercise = user;
   console.log(exercise);
 
-  window.location = "/";
+  // window.location = "/";
 
-  return <div>CreateExercise</div>;
+  return (
+    <div>
+      <h3>Create New Exercise Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+            ref="userInput"
+            required
+            className="form-control"
+            value={user.username}
+            onChange={onChangeUsername}
+          >
+            {user.users.map(function (user1) {
+              return (
+                <option key={user1} value={user1}>
+                  {user1}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={user.description}
+            onChange={onChangeDescription}
+          />
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+            type="text"
+            className="form-control"
+            value={user.duration}
+            onChange={onChangeDuration}
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            {/* <DatePicker selected={user.date} onChange={onChangeDate} /> */}
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create Exercise Log"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CreateExercise;
