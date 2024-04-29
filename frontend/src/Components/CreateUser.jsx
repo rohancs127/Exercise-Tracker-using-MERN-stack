f8283f56092afee3ae2b9563cb2dfa320e7e34a7
Rohan <rohankumar5803@gmail.com>
2024-04-29 21:52:30 +0530

    sets up ui code

--- frontend/src/Components/CreateUser.jsx
+++ frontend/src/Components/CreateUser.jsx
@@ -20,7 +20,30 @@ function CreateUser() {
     });
   };
 
-  return <div>CreateUser</div>;
+  return (
+    <div>
+      <h3>Create New User</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={user.username}
+            onChange={onChangeUsername}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create User"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CreateUser;
