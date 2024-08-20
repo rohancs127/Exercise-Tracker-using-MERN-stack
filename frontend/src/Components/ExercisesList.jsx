21ad190043a024053c32308a1af3ffc3cec19b4a
Rohan <rohankumar5803@gmail.com>
2024-08-20 16:12:40 +0530

    add exercise list api to frontend

--- frontend/src/Components/ExercisesList.jsx
+++ frontend/src/Components/ExercisesList.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function ExercisesList() {
+  const [exerciseList, setExerciseList] = useState([]);
+
+  useEffect(()=>{
+    axios.get("http://localhost:5000/exercises")
+    .then(response =>{
+      setExerciseList(response.data)
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }, []) 
+
   return <div>ExercisesList</div>;
 }
 
