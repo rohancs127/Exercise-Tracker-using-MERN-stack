fdecebe6b252f7d66d881cd60c7deb56ba3bf079
Rohan <rohankumar5803@gmail.com>
2024-08-22 21:55:11 +0530

    crrate exercise component

---
+++ frontend/src/Components/ExerciseComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ExerciseComponent() {
+  return (
+    <div>
+      <div>User: Name</div>
+      <div>Exercise: Desc</div>
+      <div>Duration: 00</div>
+      <div>Date: dd/mm/yyyy</div>
+    </div>
+  );
+}
+
+export default ExerciseComponent;
