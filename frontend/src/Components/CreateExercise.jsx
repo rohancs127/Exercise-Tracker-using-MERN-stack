812f704cfeb218e2939e2ea8ac7102b75b9a5248
Rohan <rohankumar5803@gmail.com>
2024-08-23 22:28:48 +0530

    add style for create exercise component

--- frontend/src/Components/CreateExercise.jsx
+++ frontend/src/Components/CreateExercise.jsx
@@ -43,7 +43,8 @@ function CreateExercise() {
   };
 
   useEffect(() => {
-    axios.get("http://localhost:5000/users/")
+    axios
+      .get("http://localhost:5000/users/")
       .then((response) => {
         if (response.data.length > 0) {
           setUser((prevState) => ({
@@ -78,61 +79,86 @@ function CreateExercise() {
   };
 
   return (
-    <div>
-      <h3>Create New Exercise Log</h3>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label>Username: </label>
-          <select
-            ref={userInputRef}
-            required
-            className="form-control"
-            value={user.username}
-            onChange={onChangeUsername}
-          >
-            {user.users.map(function (user1) {
-              return (
-                <option key={user1} value={user1}>
-                  {user1}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <div className="form-group">
-          <label>Description: </label>
-          <input
-            type="text"
-            required
-            className="form-control"
-            value={user.description}
-            onChange={onChangeDescription}
-          />
-        </div>
-        <div className="form-group">
-          <label>Duration (in minutes): </label>
-          <input
-            type="text"
-            className="form-control"
-            value={user.duration}
-            onChange={onChangeDuration}
-          />
-        </div>
-        <div className="form-group">
-          <label>Date: </label>
-          <div>
-            <DatePicker selected={user.date} onChange={onChangeDate} />
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card">
+            <div className="card-header bg-dark text-white text-center">
+              <h3 className="card-title mb-0">Create New Exercise Log</h3>
+            </div>
+            <div className="card-body">
+              <form onSubmit={onSubmit}>
+                <div className="form-group mb-3">
+                  <label htmlFor="username" className="form-label">
+                    Username:
+                  </label>
+                  <select
+                    ref={userInputRef}
+                    required
+                    className="form-select"
+                    value={user.username}
+                    onChange={onChangeUsername}
+                    id="username"
+                  >
+                    {user.users.map(function (user1) {
+                      return (
+                        <option key={user1} value={user1}>
+                          {user1}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+                <div className="form-group mb-3">
+                  <label htmlFor="description" className="form-label">
+                    Description:
+                  </label>
+                  <input
+                    type="text"
+                    required
+                    className="form-control"
+                    value={user.description}
+                    onChange={onChangeDescription}
+                    id="description"
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <label htmlFor="duration" className="form-label">
+                    Duration (in minutes):
+                  </label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    value={user.duration}
+                    onChange={onChangeDuration}
+                    id="duration"
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <label htmlFor="date" className="form-label">
+                    Date:
+                  </label>
+                  <div>
+                    <DatePicker
+                      selected={user.date}
+                      onChange={onChangeDate}
+                      className="form-control"
+                      id="date"
+                    />
+                  </div>
+                </div>
+                <div className="form-group text-center">
+                  <input
+                    type="submit"
+                    value="Create Exercise Log"
+                    className="btn btn-success"
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-
-        <div className="form-group">
-          <input
-            type="submit"
-            value="Create Exercise Log"
-            className="btn btn-primary"
-          />
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
