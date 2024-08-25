fbf22dabbbd74f21a1b23e73869de4dcee5a892d
Rohan <rohankumar5803@gmail.com>
2024-08-25 23:09:16 +0530

    style create user form

--- frontend/src/Components/CreateUser.jsx
+++ frontend/src/Components/CreateUser.jsx
@@ -25,27 +25,40 @@ function CreateUser() {
   };
 
   return (
-    <div>
-      <h3>Create New User</h3>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label>Username: </label>
-          <input
-            type="text"
-            required
-            className="form-control"
-            value={user.username}
-            onChange={onChangeUsername}
-          />
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header bg-info text-white">
+              <h3 className="card-title mb-0">Create New User</h3>
+            </div>
+            <div className="card-body">
+              <form onSubmit={onSubmit}>
+                <div className="form-group mb-3">
+                  <label htmlFor="username" className="form-label">
+                    Username:
+                  </label>
+                  <input
+                    type="text"
+                    required
+                    className="form-control"
+                    value={user.username}
+                    onChange={onChangeUsername}
+                    id="username"
+                  />
+                </div>
+                <div className="form-group text-center">
+                  <input
+                    type="submit"
+                    value="Create User"
+                    className="btn btn-success"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="form-group">
-          <input
-            type="submit"
-            value="Create User"
-            className="btn btn-primary"
-          />
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
